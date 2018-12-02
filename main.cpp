fd640954751332e96f221bf8257bc466413b490c
Roman Chomik <romanchom@gmail.com>
2018-12-02 20:07:17 +0100

    Example communication with magnetometer.

--- main.cpp
+++ main.cpp
@@ -6,26 +6,48 @@
 #include "MPU-9255-driver/Registers.hpp"
 
 
-MbedAsyncI2C<4, 64> i2c(PB_9, PB_8);
+MbedAsyncI2C<4, 128> i2c(PB_9, PB_8);
 
 DigitalOut led1(LED1);
 
+constexpr uint8_t agAddress = 104;
+constexpr uint8_t magAddress = 12;
+
+namespace AG = AccelerometerGyroscope;
+
 int main() {
     printf("Hello\n\r");
-    int i = 0;
+
+    // enable direct i2c access to magnetometer
+    AG::InterruptBypassConfiguration config;
+    config.enableBypass = true;
+    i2c.scheduleWrite(agAddress, config, [](){});
+    wait(0.1);
+
+    // enable continous measurement
+    Magnetometer::Control1 control = {};
+    control.mode = Magnetometer::OperationMode::continousMeasurement2;
+    i2c.scheduleWrite(magAddress, control, [](){
+        printf("Magnetometer init\n\r");
+    });
+
+    wait(0.1);
+
     while (true) {
         led1 = !led1;
-        printf("\n\n", ++i);
-        i2c.schedule<TemperatureRegister>(I2CTransferType::read, 104, [](auto const& data){
-            printf("Temperature1 %f\n\r", data.value());
+        printf("\n\n");
+        i2c.scheduleRead<AG::TemperatureRegister>(agAddress, [](auto const& data){
+            printf("Temperature1 %d\n\r", data.value());
         });
-        i2c.schedule<AccelerometerMeasurements>(I2CTransferType::read, 104, [](auto const& data){
-            printf("Accelerometer %f %f %f\n\r", data.x(), data.y(), data.z());
+        i2c.scheduleRead<AG::AccelerometerMeasurements>(agAddress, [](auto const& data){
+            printf("Accelerometer %d %d %d\n\r", data.x(), data.y(), data.z());
         });
-        i2c.schedule<GyroscopeMeasurements>(I2CTransferType::read, 104, [](auto const& data){
-            printf("Gyroscope %f %f %f\n\r", data.x(), data.y(), data.z());
+        i2c.scheduleRead<AG::GyroscopeMeasurements>(agAddress, [](auto const& data){
+            printf("Gyroscope %d %d %d\n\r", data.x(), data.y(), data.z());
         });
-
-        wait(0.9);
+        i2c.scheduleRead<Magnetometer::Measurements>(magAddress, [](auto const& data){
+            printf("Magnetometer %d %d %d\n\r", data.x(), data.y(), data.z());
+        });
+        wait(1.0);
     }
 }
